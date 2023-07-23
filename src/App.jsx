54c489ca95816b249d6c45e9e9e1bbca74ef2ebe
Omar S <osjpsi@yandex.com>
2023-07-22 20:04:53 -0600

    context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import AppRouter from "./router/AppRouter";
-
+import AuthState from "./context/AuthState";
+import ProductState from "./context/ProductState";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
 
 const App = () => {
   return (
     <>
-      <AppRouter />
+      <AuthState>
+        <ProductState>
+          <AppRouter />
+        </ProductState>
+      </AuthState>
     </>
   );
 };
---
+++ src/context/ProductReducer.js
@@ -0,0 +1,30 @@
+const reducer = (globalState, action) => {
+  switch (action.type) {
+    case "OBTENER_PRODUCTOS":
+      return {
+        ...globalState,
+        products: action.payload,
+      };
+
+    case "OBTENER_PRODUCTO":
+      return {
+        ...globalState,
+        product: action.payload,
+      };
+
+    case "AGREGAR_PRODUCTO":
+      return {
+        ...globalState,
+        cart: globalState.cart.find(
+          (product) => product.id === action.payload.id
+        )
+          ? [...globalState.cart]
+          : [...globalState.cart, action.payload],
+      };
+
+    default:
+      return globalState;
+  }
+};
+
+export default reducer;
